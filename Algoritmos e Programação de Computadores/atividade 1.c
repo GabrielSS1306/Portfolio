e43faa432a8e722dd2a7cb674b04ba5a873fb043
Gabriel <gabriel.santossoares@sempreceub.com>
2025-10-16 14:30:25 -0300

    APC/Código 1

---
+++ Algoritmos e Programação de Computadores/atividade 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    /* Nosso primeiro códigdo */
+    printf("Hello World\n");
+
+    int a, b;
+    float soma;
+
+    a = 10;
+    b = 20;
+
+    soma = a+b;
+    printf("A soma dos valores e igual a: %f", soma);
+
+    return 0;
+}

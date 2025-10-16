3bfdc89407812c9e8d315db76a607784913b7e0e
Gabriel <gabriel.santossoares@sempreceub.com>
2025-10-16 14:34:53 -0300

    APC/Código 2

---
+++ Algoritmos e Programação de Computadores/atividade 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x, y;
+
+    printf("Digite o primeiro valor: ");
+    scanf("%d",&x);
+    printf("Digite o segundo valor: ");
+    scanf("%d",&y);
+    printf("A soma de %d + %d = %d", x, y, x+y);    
+    return 0;
+}
